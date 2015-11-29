7ad6e0ed12e7910ff88f68274a9ee7f66b4c0d1c
naved92 <imtiaznaved@gmail.com>
2015-11-29 18:34:22 +0600

    index url edit

--- testsharethis/urls.py
+++ testsharethis/urls.py
@@ -20,7 +20,7 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^/$', testshare.index),
+    url(r'^/$', include('testshare.urls')),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^testshare/', include('testshare.urls')),
 
