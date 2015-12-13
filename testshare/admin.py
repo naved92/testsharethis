079038539089cfc8864a1bfca6c81b8697e8d180
naved92 <imtiaznaved@gmail.com>
2015-12-13 10:45:22 +0600

    database modified

--- testshare/admin.py
+++ testshare/admin.py
@@ -1,9 +1,8 @@
 from django.contrib import admin
-from testshare.models import UserProfile,Location,Log,Post,Block
+from testshare.models import UserProfile,Location,Post,Block
 
 admin.site.register(UserProfile)
 admin.site.register(Location)
-admin.site.register(Log)
 admin.site.register(Post)
 admin.site.register(Block)
 
